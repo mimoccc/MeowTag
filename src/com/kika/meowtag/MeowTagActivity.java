bbd633c98e3341d5358043ef8e4d4af2d20f5537
Aleksandar D. Balalovski <abalalovski@gmail.com>
2012-01-04 23:59:31 +0100

    User location handling

--- src/com/kika/meowtag/MeowTagActivity.java
+++ src/com/kika/meowtag/MeowTagActivity.java
@@ -1,11 +1,16 @@
 package com.kika.meowtag;
 
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.SimpleLocationOverlay;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MeowTagActivity extends Activity {
@@ -22,13 +27,32 @@ public class MeowTagActivity extends Activity {
         mMapView = (MapView) findViewById(R.id.map_view);
         
         MapController mapController = mMapView.getController();
-        mapController.setZoom(2);
+        mapController.setZoom(15);
         
         mMapView.setBuiltInZoomControls(true);
         mMapView.setMultiTouchControls(true);
         mMapView.setTileSource(TileSourceFactory.MAPNIK);
         
         mMyLocationOverlay = new SimpleLocationOverlay(this);
-        mMapView.getOverlays().add(this.mMyLocationOverlay);
+        
+        mMapView.getOverlays().add(mMyLocationOverlay);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocListenner);
     }
+	
+	LocationListener mLocListenner = new LocationListener() {
+	    public void onLocationChanged(Location location) {
+	    	double lat = location.getLatitude();
+	    	double lng = location.getLongitude();
+	    	
+	    	mMapView.getController().animateTo(lat, lng);
+	    }
+
+	    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+	    public void onProviderEnabled(String provider) {}
+
+	    public void onProviderDisabled(String provider) {}
+	  };
 }
